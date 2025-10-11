a8bd85ab0d5d5ffca9d03e1da8d71c87c62f8020
Charlesss-0 <aragonaugusto588@gmail.com>
2025-10-11 14:03:35 -0600

    feat: add supabase auth

---
+++ src/app/protected/page.tsx
@@ -0,0 +1,21 @@
+import { LogoutButton } from '@/components/logout-button';
+import { createClient } from '@/utils/server';
+import { redirect } from 'next/navigation';
+
+export default async function ProtectedPage() {
+	const supabase = await createClient();
+
+	const { data, error } = await supabase.auth.getClaims();
+	if (error || !data?.claims) {
+		redirect('/auth/login');
+	}
+
+	return (
+		<div className="flex h-svh w-full items-center justify-center gap-2">
+			<p>
+				Hello <span>{data.claims.email}</span>
+			</p>
+			<LogoutButton />
+		</div>
+	);
+}
