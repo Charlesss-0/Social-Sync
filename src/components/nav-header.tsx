d21ce2041201a42ddd4e166055aa9c91517a28a5
Charlesss-0 <aragonaugusto588@gmail.com>
2025-11-27 22:03:55 -0600

    refactor: remove unused auth provider and hooks; update button styles

--- src/components/nav-header.tsx
+++ src/components/nav-header.tsx
@@ -1,16 +1,13 @@
 'use client';
 
-import useUserStore from '@/stores/auth-store';
 import { type JSX } from 'react';
 
 export default function NavHeader(): JSX.Element {
-	const { user } = useUserStore();
-
 	return (
 		<header className="flex border-b border-foreground">
 			<div>Calendar</div>
 			<div>Content</div>
-			<div>Welcome, {user?.email}</div>
+			<div>Welcome, </div>
 		</header>
 	);
 }
